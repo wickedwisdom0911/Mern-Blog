56804d992f362f726f007dd8b9ecb3e516ff55aa
wckedwisdom0911 <shivamrawat091125@gmail.com>
2024-04-25 00:18:48 +0530

    sign in api route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { signup } from "../controllers/auth.controller.js";
+import { signup,signin } from "../controllers/auth.controller.js";
 
 
 const router=express.Router();
@@ -7,7 +7,7 @@ const router=express.Router();
 
 
 router.post('/signup',signup);
-
+router.post('/signin',signin);
 
 
 export default router;
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Dashboard from './pages/Dashboard'
 import Header from './components/Header'
+import FooterCom from './components/Footer'
 
 function App() {
   return (
@@ -19,7 +20,7 @@ function App() {
   <Route path='/dashboard' element={<Dashboard/>}/>
   </Routes>
  
- 
+ <FooterCom/>
  </BrowserRouter>
   )
 }
