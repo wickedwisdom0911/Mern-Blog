191641d3e0e9b1f0f8a9a1a6aaf557c69482f994
wckedwisdom0911 <shivamrawat091125@gmail.com>
2024-05-13 19:00:56 +0530

    upload functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,7 +17,9 @@ function App() {
   <Route path='/about' element={<About/>}/>
   <Route path='/sign-in' element={<SignIn/>}/>
   <Route path='/sign-up' element={<SignUp/>}/>
-  <Route path='/dashboard' element={<Dashboard/>}/>
+  <Route element={<PrivateRoute />}>
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Route>
   </Routes>
  
  <FooterCom/>
