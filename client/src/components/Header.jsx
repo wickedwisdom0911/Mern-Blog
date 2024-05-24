f341236ba43891081bd253e63efd979c6dd5be99
wckedwisdom0911 <shivamrawat091125@gmail.com>
2024-05-24 16:53:02 +0530

    s

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,7 +5,7 @@ import { FaMoon, FaSun } from 'react-icons/fa';
 import { useSelector, useDispatch } from 'react-redux';
 import { toggleTheme } from '../redux/theme/themeSlice';
 import { signoutSuccess } from '../redux/user/userSlice';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Header() {
   const path = useLocation().pathname;
